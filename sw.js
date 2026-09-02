99ece8d4cf50817823b6acd5d45ac058aeacc0ea
orbicorporate <orbicorporate@gmail.com>
2026-09-02 01:12:00 -0300

    chore(sw): bump cache v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cultivai-v6';
+const CACHE='cultivai-v7';
 const ASSETS=['/','/index.html','/manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k!=='cultivai-shared').map(k=>caches.delete(k)))));self.clients.claim();});
